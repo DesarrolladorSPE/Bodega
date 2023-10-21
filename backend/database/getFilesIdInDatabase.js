5cc1d7899c69ab14cc07bc6c1f48ae6d6560a190
5arSanti <santiari05@hotmail.com>
2023-10-21 08:51:53 -0500

    Beginning of filtering of each record for insertion into the database

---
+++ backend/database/getFilesIdInDatabase.js
@@ -0,0 +1,17 @@
+const connection = require("./index");
+
+const getFileIdInDatabase = async () => {
+    const query = "SELECT id FROM reportes";
+    return new Promise((resolve, reject) => {
+        connection.query(query, (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                const existingIds = results.map(result => result.id);
+                resolve(existingIds);
+            }
+        });
+    });
+}
+
+module.exports = getFileIdInDatabase;
