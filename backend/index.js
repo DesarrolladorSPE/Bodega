571075e32897d45bf0a4182ddbb045ea033983b6
5arSanti <santiari05@hotmail.com>
2023-12-11 21:51:32 -0500

    Changes for production

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const routerApi = require("./routes");
 
 //APP and Port
 const app = express();
-const port = 3080;
+const port = 15106;
 
 app.use(express.json());
 
@@ -14,7 +14,8 @@ const whiteList = [
 	"http://localhost:3000",
 	"http://127.0.0.1:5500",
 	"http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+	"http://10.140.0.16:15106/consolidainfo"
 ];
 const options = {
     origin: (origin, callback) => {
